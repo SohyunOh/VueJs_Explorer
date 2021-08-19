404aa59f7993cc181bb151d064ab6ed8a01f59d7
unknown <612thgus@naver.com>
2021-08-19 16:14:03 +0900

    vue.js methods  활용

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,67 @@
 
 <template>
-  <div>
-    <template v-for="(animal, index) in animals" :key="index">
-      <h2 v-if="animal !=='monkey'">{{animal}} 인덱스는 :: {{index}}</h2>
-    </template>
-  </div> 
+  <div id="App">
+    <!-- <h1>{{conut}}<h1>
+    <button>증가</button> -->
+    <!-- <h2 v-for="(animal, index) in animals" :key="index">{{animal}}</h2>
+    <hr>
+    <ul>
+      <li v-for="(user, index) in users" :key="index">
+        이름 : {{user.name}} , 직업 : {{user.job}}
+        <p v-for="item in user.skill" :key="item"> {{item}}</p>
+      </li>
+    </ul> -->
+    <h1>{{ count + 3 }}</h1>
+    <button v-on:click="addNumber(1)">증가</button>
+    <button v-on:click="minusNumber">감소</button><br>
+    <button v-on:mouseover="addNumber(10)">마우스오버10증가</button>
+  </div>
 </template>
-
+    
 <script>
-// {} 사용
 export default {
-  name : "App",
+  name: "App",
   data() {
     return {
-      age : 10,
-      display : false,
-      animals : ["monkey","rat","dog", "lion","monkey"],
+      age: 10,
+      display: false,
+      animals: ["monkey", "rat", "dog", "lion", "monkey"],
       users: [
-        {name: 'scalper', job : 'dverloper', gender: 'male',skill:["html", "css", "javaScript"] },
-        {name: 'john', job : 'desigener', gender: 'male',skill:["html", "css", "javaScript"] },
-        {name: '희수', job : 'pm', gender: 'female',skill:["html", "css", "javaScript"] }
-      ]
-      }
+        {
+          name: "scalper",
+          job: "dverloper",
+          gender: "male",
+          skill: ["html", "css", "javaScript"],
+        },
+        {
+          name: "john",
+          job: "desigener",
+          gender: "male",
+          skill: ["html", "css", "javaScript"],
+        },
+        {
+          name: "희수",
+          job: "pm",
+          gender: "female",
+          skill: ["html", "css", "javaScript"],
+        },
+      ],
+      count: 0,
+    };
+  },
+  methods: {
+    addNumber(value){
+ console.log(value)
+      this.count = this.count+ value;
+    },
+    minusNumber(){
+      console.log(this)
+      this.count = this.count-1;
     }
-  
-  };
-</script>
+  },
+};
 
-<!-- <div id="content" class="content"> -->
-        <!-- 라우터 부분만 재 로딩  -->
-        <!-- <router-view></router-view>  -->
-    <!-- </div> -->
+</script>
 
 <style>
 #App {
@@ -38,8 +69,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  padding: 10px;
+  color: black;
+  margin-top: 60px;
+}
+#App>button {
+  width:100px;
+  height:100px;
+  font-size:24px;
 }
 
 input {
@@ -51,8 +87,8 @@ input {
 .salmon {
   color: salmon;
 }
-.red{
- color: red;
+.red {
+  color: red;
 }
 .not-good {
   text-decoration: line-through;
